f8f333312d2310891ae822596c731ad36780adb0
premkumar8686 <premkumarweb0369@gmail.com>
2023-05-01 19:17:27 +0530

    First Commite

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,17 @@
+import fakeStoreAPI from "../../apis/fakeStoreAPI";
+import { ActionTypes } from "../constants/action-types";
+
+export const fetchProducts = () => {
+  return async function (dispatch) {
+    const response = await fakeStoreAPI.get("/products");
+    dispatch(setProducts(response.data));
+  };
+};
+
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  };
+};
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { mainReducer } from "./reducers/productReducer";
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const myStore = createStore(
+  mainReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default myStore; 
